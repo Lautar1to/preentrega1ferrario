99bafc121681d7041103d99ff3625bfc8bdb31d2
Lauti Ferrario <lautiferrario@gmail.com>
2023-02-20 00:58:14 -0300

    javascript final, con bucle

--- javascript.js
+++ javascript.js
@@ -1,12 +1,22 @@
-const numAlumnos = parseInt(prompt('Ingrese el número de alumnos a evaluar:'));
-
-let suma = 0;
-
-for (let i = 1; i <= numAlumnos; i++) {
-  const nota = parseFloat(prompt(`Ingrese la nota del alumno ${i}:`));
-
-  suma += nota;
-}
-
-const promedio = suma / numAlumnos;
-alert(`El promedio de notas es: ${promedio}`);
+function solicitarNotas() {
+    const numAlumnos = parseInt(prompt('Ingrese el número de alumnos a evaluar:'));
+  
+    let suma = 0;
+  
+    for (let i = 1; i <= numAlumnos; i++) {
+      const nota = parseFloat(prompt(`Ingrese la nota del alumno ${i}:`));
+  
+      suma += nota;
+    }
+  
+    const promedio = suma / numAlumnos;
+    alert(`El promedio de notas es: ${promedio}`);
+  
+    const repetir = confirm('¿Desea calcular el promedio de notas de otro grupo de alumnos?');
+    
+    if (repetir) {
+      solicitarNotas();
+    }
+  }
+  
+  solicitarNotas();
